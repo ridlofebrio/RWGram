546aed1027edafafda2906e1481eacc1c0226d02
krisna <saonoke21@gmail.com>
2024-04-04 10:41:32 +0700

    fix: laporan crud

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -66,14 +66,20 @@ class LaporanController extends Controller
     {
         $request->validate([
             'penduduk_id' => 'required',
-            'nomor_surat' => 'required',
-            'keterangan' => 'required',
-            'tanggal_persuratan' => 'required',
+            'jenis_laporan' => 'required',
+            'deskripsi_laporan' => 'required',
+            'tanggal_laporan' => 'required',
             'status_laporan' => 'required'
         ]);
 
-        LaporanModel::find($id)->update($request->all());
-        return redirect()->route('laporan.index');
+        LaporanModel::find($id)->update([
+            'penduduk_id' => $request->penduduk_id,
+            'jenis_laporan' => $request->jenis_laporan,
+            'deskripsi_laporan' => $request->deskripsi_laporan,
+            'status_laporan' => $request->status_laporan,
+            'tanggal_laporan' => $request->tanggal_laporan
+        ]);
+        return redirect('/laporan');
     }
 
     /**
