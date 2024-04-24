6c2e9b3d39828c0a53a227dcfc91ded78ee2abb9
Febrio <muhridlo111@gmail.com>
2024-04-24 12:05:00 +0700

    fix:Seedeer Insyaallah sudah nyambung semua, faker dihilangkan

--- database/seeders/InformasiSeeder.php
+++ database/seeders/InformasiSeeder.php
@@ -15,31 +15,47 @@ class InformasiSeeder extends Seeder
     {
         $data = [
             [
-                'user_id' => 1,
+                'user_id' => 2,
                 'judul' => 'Buka Bersama',
-                'deskripsi_informasi' => 'Buka bersama di rumah pak rt',
+                'deskripsi_informasi' => 'Buka bersama di rumah pak rt. Acara buka bersama ini diadakan untuk mempererat silaturahmi antar warga di lingkungan kita. Mari kita nikmati hidangan lezat sambil berbagi cerita dan kebahagiaan bersama. Semua warga diundang untuk ikut serta dalam acara ini. Jadwal acara adalah pada tanggal 25 April 2024 pukul 18.00 WIB di rumah Pak RT Budi, Jl. Lawang Barat.',
                 'foto_informasi' => '',
                 'lokasi_informasi' => 'Jl. Lawang Barat Rumah Pak RT Budi',
                 'tanggal_informasi' => now()
             ],
             [
-                'user_id' => 1,
+                'user_id' => 2,
                 'judul' => 'Kerja Bakti',
-                'deskripsi_informasi' => 'Kerja bakti lingkungan RW',
+                'deskripsi_informasi' => 'Kerja bakti lingkungan RW. Mari kita bergotong-royong untuk menjaga kebersihan dan keindahan lingkungan kita. Acara kerja bakti akan dilaksanakan pada hari Sabtu, tanggal 27 April 2024 pukul 08.00 WIB. Mari kita bersama-sama membersihkan selokan, trotoar, dan area publik lainnya agar lingkungan kita tetap bersih dan nyaman untuk semua warga.',
                 'foto_informasi' => '',
                 'lokasi_informasi' => 'Jl. Lawang Barat',
                 'tanggal_informasi' => now()
             ],
             [
-                'user_id' => 1,
+                'user_id' => 2,
                 'judul' => 'Senam Sehat',
-                'deskripsi_informasi' => 'Senam sehat bugar bersama',
+                'deskripsi_informasi' => 'Senam sehat bugar bersama. Ayo jaga kesehatan tubuh kita dengan berpartisipasi dalam acara senam sehat bugar bersama. Acara ini akan dilaksanakan setiap hari Minggu pukul 06.00 WIB di Lapangan Tennis Warga, Jl. Lawang Barat. Dengan senam yang teratur, kita bisa menjaga tubuh tetap bugar dan sehat.',
                 'foto_informasi' => '',
                 'lokasi_informasi' => 'Jl. Lawang Barat Lapangan Tennis Warga',
                 'tanggal_informasi' => now()
             ],
-
+            [
+                'user_id' => 2,
+                'judul' => 'Posyandu',
+                'deskripsi_informasi' => 'Posyandu adalah tempat untuk memeriksa kesehatan ibu dan anak secara berkala. Di Posyandu, kita dapat melakukan pemeriksaan kesehatan rutin, mendapatkan imunisasi, serta mendapatkan edukasi tentang kesehatan ibu dan anak. Mari datang dan manfaatkan layanan Posyandu untuk kesehatan keluarga kita. Jadwal Posyandu adalah setiap hari Kamis pukul 09.00 - 12.00 WIB di Lapangan Tennis Warga, Jl. Lawang Barat.',
+                'foto_informasi' => '',
+                'lokasi_informasi' => 'Jl. Lawang Barat Lapangan Tennis Warga',
+                'tanggal_informasi' => now()
+            ],
+            [
+                'user_id' => 2,
+                'judul' => 'Halal Bi Halal',
+                'deskripsi_informasi' => 'Halal bi halal merupakan tradisi silaturahmi yang dilakukan setelah lebaran. Acara ini adalah momentum untuk mempererat hubungan antarwarga serta meminta maaf atas segala kesalahan yang terjadi. Halal bi halal akan dilaksanakan pada tanggal 3 Mei 2024 pukul 10.00 WIB di Lapangan Tennis Warga, Jl. Lawang Barat. Mari kita sambut kedatangan bulan suci Syawal dengan penuh kebahagiaan dan kedamaian.',
+                'foto_informasi' => '',
+                'lokasi_informasi' => 'Jl. Lawang Barat Lapangan Tennis Warga',
+                'tanggal_informasi' => now()
+            ],
         ];
+        
         DB::table('informasi')->insert($data);
     }
 }
--- database/seeders/LaporanSeeder.php
+++ database/seeders/LaporanSeeder.php
@@ -14,21 +14,59 @@ class LaporanSeeder extends Seeder
      */
     public function run(): void
     {
-        $faker = Factory::create();
-        $keluhan = array('keluhan', 'pengajuan', 'entah');
 
-        for ($i = 1; $i <= 3; $i++) {
+        $data = [
+            [
+                'laporan_id' => 1,
+                'penduduk_id' => 1,
+                'jenis_laporan' => 'keluhan',
+                'deskripsi_laporan' => 'Saya ingin melaporkan tentang masalah sanitasi di lingkungan kami. Beberapa hari terakhir, kami telah mengalami masalah dengan saluran air yang tersumbat di sekitar rumah kami. Hal ini menyebabkan air kotor dan limbah tersumbat di depan rumah kami, menciptakan bau yang tidak sedap dan lingkungan yang tidak sehat. Kami sangat berharap masalah ini bisa segera ditangani oleh pihak yang berwenang. Kami juga membutuhkan perbaikan lampu jalan di sekitar wilayah kami, karena beberapa lampu sudah mati dan membuat malam hari menjadi gelap dan tidak aman bagi kami yang harus pulang larut malam. Terima kasih atas perhatiannya.',
+                'status_laporan' => 'Menunggu',
+                'tanggal_laporan' => now(),
+                'created_at' => now()
+            ],
+            [
+                'laporan_id' => 2,
+                'penduduk_id' => 2,
+                'jenis_laporan' => 'keluhan',
+                'deskripsi_laporan' => 'Saya ingin melaporkan adanya masalah dengan fasilitas kesehatan di lingkungan kami. Rumah sakit terdekat sudah tidak mampu menampung jumlah pasien yang semakin meningkat, menyebabkan antrian panjang dan pelayanan yang kurang optimal. Selain itu, kami juga mengalami kesulitan dalam mendapatkan obat-obatan dan perawatan medis yang diperlukan. Kami membutuhkan perhatian dan solusi dari pemerintah terkait masalah ini agar masyarakat bisa mendapatkan pelayanan kesehatan yang memadai dan terjangkau. Terima kasih.',
+                'status_laporan' => 'Menunggu',
+                'tanggal_laporan' => now(),
+                'created_at' => now()
+            ],
+            [
+                'laporan_id' => 3,
+                'penduduk_id' => 3,
+                'jenis_laporan' => 'keluhan',
+                'deskripsi_laporan' => 'Saya ingin melaporkan tentang keamanan di lingkungan kami yang semakin memprihatinkan. Beberapa minggu terakhir, kami sering mendengar tentang kasus pencurian dan tindak kriminal lainnya yang terjadi di sekitar kami. Kami merasa khawatir akan keselamatan kami dan keluarga kami. Kami membutuhkan peningkatan patroli keamanan dan peningkatan sinar lampu jalan di sekitar wilayah kami untuk mencegah tindak kriminal tersebut. Kami harap pihak berwenang segera mengambil tindakan untuk meningkatkan keamanan lingkungan kami. Terima kasih.',
+                'status_laporan' => 'Menunggu',
+                'tanggal_laporan' => now(),
+                'created_at' => now()
+            ],
+            [
+                'laporan_id' => 4,
+                'penduduk_id' => 4,
+                'jenis_laporan' => 'keluhan',
+                'deskripsi_laporan' => 'Saya ingin melaporkan tentang masalah transportasi publik di wilayah kami yang semakin buruk. Kami sering mengalami keterlambatan dan ketidakpastian dalam jadwal transportasi publik yang ada. Hal ini menyulitkan kami untuk bekerja dan beraktivitas sehari-hari. Selain itu, kondisi armada transportasi yang sudah tua dan kurang terawat juga menyebabkan ketidaknyamanan dan risiko keselamatan bagi penumpang. Kami berharap pihak terkait bisa segera melakukan perbaikan dan peningkatan dalam sistem transportasi publik di wilayah kami. Terima kasih.',
+                'status_laporan' => 'Menunggu',
+                'tanggal_laporan' => now(),
+                'created_at' => now()
+            ],
+            [
+                'laporan_id' => 5,
+                'penduduk_id' => 5,
+                'jenis_laporan' => 'keluhan',
+                'deskripsi_laporan' => 'Saya ingin melaporkan tentang masalah kebersihan di lingkungan kami. Beberapa kali kami melihat sampah berserakan di sekitar tempat pembuangan sampah yang sudah penuh. Hal ini menyebabkan bau tidak sedap dan menarik penyebaran hama serta penyakit. Kami juga menemukan banyak jalan yang tidak bersih dan terdapat genangan air yang menjadi sarang nyamuk. Kami berharap pihak terkait segera melakukan tindakan untuk membersihkan lingkungan kami dan mengatasi masalah ini. Terima kasih atas perhatiannya.',
+                'status_laporan' => 'Menunggu',
+                'tanggal_laporan' => now(),
+                'created_at' => now()
+            ],
+        ];
+        
+
             DB::table('laporan')->insert(
-                [
-                    'laporan_id' => $i,
-                    'penduduk_id' => 1,
-                    'jenis_laporan' => $keluhan[$i - 1],
-                    'deskripsi_laporan' => $faker->sentence(),
-                    'status_laporan' => 'Menunggu',
-                    'tanggal_laporan' => now(),
-                    'created_at' => now()
-                ]
+                $data
             );
-        }
+        
     }
 }
