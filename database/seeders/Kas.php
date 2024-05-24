f0a43fd6e54f68b21b9a9724f1d2e151ee16e419
Febrio <muhridlo111@gmail.com>
2024-05-24 19:11:37 +0700

    feat : tabel kas dan seeder kas log

--- database/seeders/Kas.php
+++ database/seeders/Kas.php
@@ -17,26 +17,31 @@ class Kas extends Seeder
         $data = [
             [
                 'kartu_keluarga_id' => 1,
+                'pengelola' => 'RT 01',
                 'tahun' => 2024,
                 'Januari' => true
             ],
             [
                 'kartu_keluarga_id' => 2,
+                'pengelola' => 'RT 01',
                 'tahun' => 2024,
                 'Januari' => true
             ],
             [
                 'kartu_keluarga_id' => 3,
+                'pengelola' => 'RT 01',
                 'tahun' => 2024,
                 'Januari' => true
             ],
             [
                 'kartu_keluarga_id' => 4,
+                'pengelola' => 'RT 01',
                 'tahun' => 2024,
                 'Januari' => true
             ],
             [
                 'kartu_keluarga_id' => 5,
+                'pengelola' => 'RT 01',
                 'tahun' => 2024,
                 'Januari' => true
             ],
