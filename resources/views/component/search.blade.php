0767b5e367572cc80d967fd4bca281357bfeb250
hunn99 <dennymalikibrahim@gmail.com>
2024-05-28 12:28:42 +0700

    [chore: view pengaduan landing page]

--- resources/views/component/search.blade.php
+++ resources/views/component/search.blade.php
@@ -2,7 +2,7 @@
     <div class="absolute inset-y-0 start-0 flex items-center ml-5 pointer-events-none">
         <img src="{{ asset('asset/icon/Search.svg') }}" alt="">
     </div>
-    <input type="text" name="search"
+    <input type="text" name="search" id="searchInput"
         class="pl-14 pr-6 block w-full p-2 border-neutral-04 rounded-full bg-white focus:ring-blue-500 focus:border-blue-500 placeholder-neutral-06 text-base"
-        placeholder="Cari UMKM" required />
+        placeholder="Cari" required />
 </div>
