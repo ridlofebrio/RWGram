25aed6b9ff1299adc11d48fdfe946b5d48eceeb4
UKM Seni Theatrisic <113005391+iannstronaut@users.noreply.github.com>
2024-06-06 10:12:02 +0700

    refactor: favicon dan title dashboard page

--- resources/views/dashboard/template.blade.php
+++ resources/views/dashboard/template.blade.php
@@ -6,7 +6,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>{{ ucfirst($active) }} | {{Auth::user()->role_id == 5 ? 'RW Admin' : 'RT Admin'}}</title>
+    <link rel="icon" href="/favicon.svg" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
     <script src="https://kit.fontawesome.com/e6bc1ebaee.js" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
